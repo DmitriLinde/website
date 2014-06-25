f468d858e916636a023a856b9ff6c67c7dc5d140
Dmitri Linde <dlinde@aereo.com>
2014-06-25 17:59:59 -0400

    Add Resume

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,15 @@ class WelcomeController < ApplicationController
   $headers = {
     'About' => '/',
     'Projects' => '/projects',
+    'Resume' => '/resume',
     'Contact' => '/contact' }
 
   def about
     @header = "About"
+  end
+
+  def resume
+    @header = 'Resume'
   end
 
   def contact
